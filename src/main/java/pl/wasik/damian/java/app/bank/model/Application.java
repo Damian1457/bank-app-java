da8135c291920bd4843000bf2215f18b90266094
Damian1457 <damianwasik.coach@gmail.com>
2023-02-18 15:54:06 +0100

    Added new functionality in the application

--- src/main/java/pl/wasik/damian/java/app/bank/model/Application.java
+++ src/main/java/pl/wasik/damian/java/app/bank/model/Application.java
@@ -2,32 +2,7 @@ package pl.wasik.damian.java.app.bank.model;
 
 public class Application {
     public static void main(String[] args) {
-//        Bank damianBank = new Bank();
-//        Account damianAccount = new Account();
-//        Account jacekAccount = new Account();
-//
-//        Account jolaAccount = new Account();
-//
-//        System.out.println("Jola account before deposit: " + jolaAccount);
-//        jolaAccount.deposit(100);
-//        System.out.println("Damian account after deposit: " + damianAccount);
-//        System.out.println("Jola account before deposit: " + jolaAccount);
-//
-//        Transfer dinnerTransfer = new Transfer();
-////        dinnerTransfer.setAccountFrom(jolaAccount);
-////        dinnerTransfer.setAccountTo(damianAccount);
-//        //dinnerTransfer.transfer(10);
-//        System.out.println("Jola account after deposit: " + jolaAccount);
-//        System.out.println("Damian account after deposit: " + damianAccount);
-//
-////        int transferStatus = dinnerTransfer.dinnerTransfer(jolaAccount, damianAccount, 100.00);
-////        System.out.println(transferStatus);
-//
-////        TransferStatus dinnerTransferStatus = dinnerTransfer.transfer(jacekAccount, damianAccount, 100);
-//        TransferStatus dinnerTransferStatus = Transfer.transfer(jacekAccount, damianAccount, 100);
-//        System.out.println(dinnerTransferStatus.ordinal());
-//        System.out.println(dinnerTransferStatus.getName());
-//        System.out.println(dinnerTransferStatus.getCode());
+
     }
 }
 
