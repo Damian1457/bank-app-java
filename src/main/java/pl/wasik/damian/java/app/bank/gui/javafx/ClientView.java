c8922d41ee2510fba426323b8e8e95540c14eec3
Damian1457 <damianwasik.coach@gmail.com>
2023-04-26 22:51:16 +0200

    I created a method that handles the event of clicking on fields and restoring the previous value in the balance field.

--- src/main/java/pl/wasik/damian/java/app/bank/gui/javafx/ClientView.java
+++ src/main/java/pl/wasik/damian/java/app/bank/gui/javafx/ClientView.java
@@ -2,6 +2,7 @@ package pl.wasik.damian.java.app.bank.gui.javafx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -13,11 +14,18 @@ public class ClientView extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CreateUserView.class.getResource("client.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Create Client");
+        stage.setTitle("Banking Transaction");
         stage.setScene(scene);
         stage.show();
 
         ClientController clientController = fxmlLoader.getController();
         clientController.setCurrentBalanceTextField();
+
+
+        Parent rootNode = scene.getRoot();
+        scene.setOnMouseClicked(event -> {
+            rootNode.requestFocus();
+            clientController.clearCurrentBalanceTextField();
+        });
     }
 }
